47c9e46a09072fabfd36f63804726f07e429b057
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-08 11:09:17 -0300

    feat: Adicionando estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import { title } from 'process'
 import Head from 'next/head'
-
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -9,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello world
       </h1>
     </>
