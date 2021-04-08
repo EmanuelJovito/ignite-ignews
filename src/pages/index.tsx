1f10921a1f0a3a0a6de89359823ff0a68fd5c4b9
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-08 10:45:50 -0300

    feat: Adicionando title de maneira dinâmica por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import { title } from 'process'
+import Head from 'next/head'
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello world</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello world
+      </h1>
+    </>
   )
 }
