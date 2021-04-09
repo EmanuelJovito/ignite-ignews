d9e142b4f96897b43c46f865d6e7b314fd87e44b
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-09 14:45:19 -0300

    refactor: Transformando em SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IeJZuA9FOCg0y1cC6mdJiX2')
 
   const product = {
@@ -49,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24,
   }
 }
