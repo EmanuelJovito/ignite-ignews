abef6989aaa2b3438a87f0fc9c8d9543d089a631
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-12 10:30:56 -0300

    feat: Salvando usuarios no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,6 +13,26 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user 
 
-  database: process.env.DATABASE_URL,
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: {email}}
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+
+    }
+  }
 })
