0edcb89f98fe70bfded48f06cbac9d2a4b5fe289
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-19 14:36:48 -0300

    feat: Criando listagem de posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,23 @@ import { GetStaticProps } from 'next'
 import { getPrismicClient } from '../../services/prismic'
 import Prismic from '@prismicio/client'
 import Head from 'next/head'
-import styles from './styles.module.scss'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+import styles from './styles.module.scss'
+import { type } from 'os'
+
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string,
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <> 
       <Head>
@@ -13,21 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -44,9 +50,23 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
+
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
