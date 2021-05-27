2661ce0cfdab1813821fcfd13e935d3aefc26ffe
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-27 09:03:58 -0300

    fix: Corrigindo erro no subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publicantions <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl conding"/>
